93d6ef527865251efc674544ceeed08870bcb486
Matheus Franco <matheusefranco@gmail.com>
2020-09-16 08:56:38 -0300

    Inicio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Projeto MeuFLIX em breve aqui...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
